18220d2f8771f0c033a2e8f5272ce632ef228a6d
jayceee1207 <ablayjc7@gmail.com>
2023-07-02 23:40:06 +0800

    Program is working!

--- main.py
+++ main.py
@@ -3,3 +3,20 @@
 #July 1, 2023
 #Pet Class
 
+#import Pet class
+from Pet import Pet
+
+#ask the name of the pet
+name = input("Enter your pet's name: ")
+
+#ask the animal type
+animal_type = input("Enter what type of pet it is: ")
+
+#ask the age of the animal
+age = input("Enter your pet's age: ")
+
+#create an object
+pet = Pet(name, animal_type, age)
+
+#display the data
+pet.show()
