95fcd4d833a7db9df230280ac3526037fe541ef9
jayceee1207 <ablayjc7@gmail.com>
2023-07-02 23:16:23 +0800

    Initialize animal type, age, and name

--- Pet.py
+++ Pet.py
@@ -1,5 +1,13 @@
 #Create a class
+class Pet:
 
+#init attributes
+    def __init__(self, name, animal_type, age):
+        self.__name =  name
+        self.__animal_type = animal_type
+        self.__age = age
+
+#show result: name, type, and age
 
 #Include the data attributes
 
